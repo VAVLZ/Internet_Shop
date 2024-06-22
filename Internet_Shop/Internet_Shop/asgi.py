8f0c9a5ca8a3e243eeb5fd6f14aa03bd1480d97d
VAVLZ <ales.grechikho@gmail.com>
2024-06-22 11:19:18 +0300

    Вторая версия сайта.

--- Internet_Shop/Internet_Shop/asgi.py
+++ Internet_Shop/Internet_Shop/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Internet_Shop project.
+ASGI config for internetshop project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- Internet_Shop/Shop/views.py
+++ Internet_Shop/Shop/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Create your views here.
+
 
 def home(request):
-    return HttpResponse("Hi! This is a internet shop.")
+    return render(request, "index.html")
