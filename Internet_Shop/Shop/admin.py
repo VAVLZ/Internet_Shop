4bb7e2ca5890f2c06d33123176c1ed7269a1a333
VAVLZ <ales.grechikho@gmail.com>
2024-07-14 18:23:44 +0300

    Пятая версия сайта.

--- Internet_Shop/Shop/admin.py
+++ Internet_Shop/Shop/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from . import models
 
-
 class ProductAdmin(admin.ModelAdmin):
     pass
 
+class ReviewAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
 admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Review, ReviewAdmin)
